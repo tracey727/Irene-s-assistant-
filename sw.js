7649ec3570f6a1b803899892b54448fba8728c26
Tracey <positivity864@gmail.com>
2026-08-17 20:55:09 +1000

    Add Gigi offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='gigi-v1';
+const ASSETS=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/assets/gigi-mark.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request))));
